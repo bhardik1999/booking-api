d1b875ca80c6fd98ed5d186e326323f2910387bd
bhardik1999 <bhardik.bodakiya1999@gmail.com>
2025-07-28 18:04:15 +0530

    Added admin ui for licensce management

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const LicenseKey = require("../models/Licensekey");
+const LicenseKey = require("../models/LicenseKey");
 const crypto = require("crypto");
 
 // Admin secret
